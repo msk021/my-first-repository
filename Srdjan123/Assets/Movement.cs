5bc199e78796cecfa71f9f87de4e25e1f74a22f3
msk021 <srki1997@hotmail.com>
2022-03-29 22:24:55 +0200

    Unity1

---
+++ Srdjan123/Assets/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public int Yincrement;
+    public float speed;
+    public float maxHeight;
+    public float minHeight;   
+
+
+    // Update is called once per frame
+    void Update()
+    { 
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+        {
+
+            Pomeranje(+3);
+            /*targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            transform.position = targetPos;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime); */
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            Pomeranje(-3);
+            /*targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            transform.position = targetPos;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);*/
+        }
+
+         
+    }
+
+    void Pomeranje(int broj)
+    {
+        Vector2 targetPos = new Vector2(transform.position.x, transform.position.y + broj);
+        transform.position = targetPos;
+    }
+}
