f5c0570dd3f1826f13d7fda87c54040ad3a81d05
Marko <markoboziczv@gmail.com>
2022-04-06 22:11:50 +0200

    boza spawner
    bozaSpawner skripta i komentari na tvojoj skripti

---
+++ Srdjan123/Assets/Scripts/BozaSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BozaSpawner : MonoBehaviour
+{
+    [SerializeField] List<GameObject> obstaclePatterns;
+    [SerializeField] float timeBtwSpawn;
+    [SerializeField] float currentTimeBtwSpawn;
+    [SerializeField] float timeDecreaser;
+    [SerializeField] float minimalTime;
+
+    private void Start()
+    {
+        currentTimeBtwSpawn = timeBtwSpawn; //current time stavljamo da bude kao ovaj timebtw, jer current nismo postavili vrednost, vidi u inspektoru kako se ponasa current
+        print($"BozaSpawner: start time between spawn is {currentTimeBtwSpawn}");
+    }
+    private void Update()
+    {
+        CheckAndSpawn();
+    }
+    public void CheckAndSpawn()
+    {
+        if (currentTimeBtwSpawn > 0) //ako je current veci od 0, oduzimamo vreme
+        {
+            currentTimeBtwSpawn -= Time.deltaTime;
+        }
+        else //ako current nije veci od 0 onda se desi ovo
+        {
+            GameObject instantiatedObject = Instantiate(obstaclePatterns[Random.Range(0, obstaclePatterns.Count)]);
+            //sve unutar zagrade ovde predstavlja samo gameobject na random poziciji iz liste   obstaclePatterns[random int]
+            //nisam ga zakacio za parenta jer nema potrebe u ovom slucaju, pa mi ne treba ni quaternion
+            //nisam morao da stavim ovaj instantiatedObject = Instantiate() ali mi je lakse da bih mogao da ga stavim print
+
+            print($"BozaSpawner: {instantiatedObject.name} is spawned at {instantiatedObject.transform.position}");
+            // $"" dolar stavis da bi varijable mogao da ubacujes u tekst sa {varijabla} , inace bi morao da pises kao ovo dole
+            print("BozaSpawner: " + instantiatedObject.name + " is spawned at " + instantiatedObject.transform.position);
+            
+            if (timeBtwSpawn > minimalTime) //ako je vreme izmedju spawnovanja vece od minimalnog
+            {
+                timeBtwSpawn -= timeDecreaser; //oduzimas decreaser od timebtwspawn
+                print($"BozaSpawner: {timeBtwSpawn} is new timeBtwSpawn");
+            }
+            currentTimeBtwSpawn = timeBtwSpawn; //trenutno vreme stavljas da bude timeBtwSpawn, tj kao resetujes ga jer je doslo do 0
+                                                //al ga ne resetujes na pocetno nego si od pocetnog vec oduzeo decreaser znaci na smanjeno ga stavljas
+            print($"BozaSpawner: current time is resetted and now is {currentTimeBtwSpawn}");
+        }
+    }
+}
--- Srdjan123/Assets/Scripts/Spawner.cs
+++ Srdjan123/Assets/Scripts/Spawner.cs
@@ -6,27 +6,38 @@ public class Spawner : MonoBehaviour
 {
     public GameObject[] obstaclePattern;
 
-    private float timeBtwSpawn;
-    public float startTimeBtwSpawn;
-    public float decreaseTime;
-    public float minTime;
+    private float timeBtwSpawn; //"trenutno" vreme izmedju spawnovanja, pomalo nepotrebno u ovom slucaju
+    public float startTimeBtwSpawn; // koliko ce razmak izmedju spawnovanja biti na pocetku jer se posle menja (objasnjeno dole)
+    public float decreaseTime; //oduzima ovoliko od vremena koje si postavio svaki put kad spawnujes, znaci sad je
+                               // npr 4 sekunde razmak izmedju spawnovanja, sledeci put ce biti za ovoliko manji
+                               // znaci ako je ovo 0.1 sledeci put ce biti 3.9, 3.8, 3.7, ...
+    public float minTime; //kada dodje do ovoliko sekundi onda odradi sta treba,
+                          //stavljas minimal time da ne bi oduzimao dok dodje do 0 nego si uvek siguran  da ce biti razmak izmedju spawnovanja
 
-    // Update is called once per frame
     void Update()
     {
-        if (timeBtwSpawn <= 0)
+        if (timeBtwSpawn <= 0) //ako je vreme isteklo odradi se ovo unutra
         {
-            int rand = Random.Range(0, obstaclePattern.Length);
-            Instantiate(obstaclePattern[rand], transform.position, Quaternion.identity);
-            timeBtwSpawn = startTimeBtwSpawn;
-            if (startTimeBtwSpawn > minTime)
+            int rand = Random.Range(0, obstaclePattern.Length); //generises random broj od 0 do kolko ima clanova u listi obstaclePatterns
+            Instantiate(obstaclePattern[rand], transform.position, Quaternion.identity); //obsPattern[rand/neki broj] znaci izabrao si gameobject koji je na toj poziciji u listi,
+                                            // parrent na koji kacis objekat koji spawnujes, u ovom slucaju objekat na koji je skripta zakacena,
+                                            // mogao si napisati i this.transform.position, ili this.gameobject.transform.position ako bi ti tako bilo jasnije ali je nepotrebno,
+                                            // quaternion identity mi nikad nije bas jasno bilo sta znaci al to je vld da gameobject zadrzi svoju rotaciju koja mu je u prefabu
+                                            // jer ga ti kacis na parenta a kad to radis onda pokupi osobine parenta tipa scale i jos neke nz, nmp kad ovako radim uvek samo napisem quat.iden i tjt hahaha
+            
+            //ovo sa instantiate si mogao i samo gameobject koji oces, bez parenta i quaterniona, vidi u mojoj skripti
+            
+            timeBtwSpawn = startTimeBtwSpawn; // stavljas da ti je "trenutno" vreme izmedju spawnovanja = "pocetnom"
+            if (startTimeBtwSpawn > minTime) //ako je "pocetno" vece od minimalnog
             {
-                startTimeBtwSpawn -= decreaseTime;
+                startTimeBtwSpawn -= decreaseTime; //pocetno umanjujes za decrease time, znaci sledeci put kad se odradi linija 26
+                                                   //onda ce "trenutno" timeBtwSpawn biti ovoliko kolko bude ovo sto si upravo umanjio
 
             }
-        } else
+        }
+        else //ako vreme nije isteklo oduzimas vreme dok ne istekne
         {
-            timeBtwSpawn -= Time.deltaTime;
+            timeBtwSpawn -= Time.deltaTime; 
         }        
     }
 }
